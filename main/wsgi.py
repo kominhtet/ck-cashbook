0ef3f69fd7dddf4a880d61555246d58ee890f01f
CK <ck@example.com>
2025-10-23 23:53:37 +0630

    first commit

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:main/wsgi.py
 """
 WSGI config for main project.
 
@@ -15,3 +16,22 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
 app = application
+=======
+"""
+WSGI config for main project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+
+application = get_wsgi_application()
+app = application
+>>>>>>> bc36083 (update mobile ui):main/main/wsgi.py
